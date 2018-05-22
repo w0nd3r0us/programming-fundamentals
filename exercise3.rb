b823961e48d06bfbf9ed33eae848269e6e6e34b5
w0nd3r0us <steven.grewal@gmail.com>
2018-05-22 15:47:35 -0400

    finished adding age calculator

--- exercise3.rb
+++ exercise3.rb
@@ -2,3 +2,8 @@
 puts "what is your name?"
 user_name = gets.chomp
 puts "Hello, #{user_name}"
+
+puts "which year were you born in?"
+year = gets.chomp
+age_calc = 2017 - year.to_i
+puts "you are #{age_calc}"
