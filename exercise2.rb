89a2d83dca7eed3b8430a9be100cf950b006c3e6
w0nd3r0us <steven.grewal@gmail.com>
2018-05-22 15:18:52 -0400

    75% completed ex.2

---
+++ exercise2.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/env ruby -wKU
+#added code using string interpolation
+puts "a good tip of 15% for a $50 meal is $#{55 * 0.15}"
+#converted intiger to string using to_s
+puts "powerman" + 5000.to_s
+#adding code to a sentence using string interpolation
+puts "the result of 45628 multiplied by 7839 is #{45628 * 7839}"
